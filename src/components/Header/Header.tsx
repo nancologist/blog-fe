007db2a92f0916800decb1c266478b04b7fe8cff
nancologist <nancologist@gmail.com>
2021-12-06 01:45:15 +0100

    Update to NavLink

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,34 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
   return (
     <header>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
+      <nav className="nav">{navItems.map(item => 
+          <NavLink
+            to={item.path}
+            className={({ isActive }) => [
+              'nav__item',
+              isActive ? 'active' : null
+            ].join(' ')}
+          >
+            {item.label}
+          </NavLink>
+        )}
       </nav>
     </header>
   );
 };
+
+const navItems = [
+  {
+    label: 'Home',
+    path: '/'
+  },
+  {
+    label: 'About',
+    path: '/about'
+  }
+]
 
 export default Header;
