f00a5a81b116d73d804c3985db8e5278b6e9e169
nancologist <nancologist@gmail.com>
2022-01-06 11:58:27 +0100

    Add drafjs cmp

---
+++ src/components/TextEditor/TextEditor.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { Editor, EditorState, RichUtils } from 'draft-js';
+import 'draft-js/dist/Draft.css';
+
+const TextEditor = () => {
+  const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
+
+  const handleKeyCommand = (command: any, editorState: any) => {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+
+    if (newState) {
+      setEditorState(newState);
+      return 'handled';
+    }
+
+    return 'not-handled';
+  };
+
+
+  
+  const logIt = () => {
+    console.log(editorState);
+  };
+
+  return (
+    <>
+      <button onClick={logIt}>LOG</button><br /><br />
+
+      <Editor
+        editorState={editorState}
+        handleKeyCommand={handleKeyCommand}
+        onChange={setEditorState}
+        placeholder="Write some text..."
+      />
+    </>
+  );
+}
+
+export default TextEditor;
