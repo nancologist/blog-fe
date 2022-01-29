134eeca7feaa7b8bc39e883a99f69f9259ec4f78
nancologist <nancologist@gmail.com>
2022-01-29 23:15:51 +0100

    Fix edit post

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,9 +12,6 @@ const Header = () => {
   return (
     <header className={isDark ? 'dark' : undefined }>
       <nav className="nav">
-        <div className="logo">
-          <h1>Hello Logo</h1>
-        </div>
         <div className="nav__items">
           {navItems.map(item => {
             if (item.label === 'Admin' && !isAuth) {
@@ -42,11 +39,27 @@ const Header = () => {
 
 const navItems: NavItem[] = [
   {
-    label: 'Home',
+    label: 'Startseite',
     path: '/'
   },
   {
-    label: 'About',
+    label: 'Bindung und Erziehung',
+    path: '/bonding-and-upbringing'
+  },
+  {
+    label: 'Glaube',
+    path: '/belief'
+  },
+  {
+    label: 'Ernährung',
+    path: '/diet'
+  },
+  {
+    label: 'Wohnliches',
+    path: '/livable'
+  },
+  {
+    label: 'Über mich',
     path: '/about'
   },
   {
