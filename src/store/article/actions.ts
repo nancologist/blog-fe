3388c40ee3c28d98363d816b573eb6024a9710ac
nancologist <nancologist@gmail.com>
2022-02-02 21:45:58 +0100

    Add Redux Thunk (Toolkit) for fetching all articles

--- src/store/article/actions.ts
+++ src/store/article/actions.ts
@@ -1,6 +1,15 @@
-import { createAction } from '@reduxjs/toolkit';
+import { createAction, createAsyncThunk } from '@reduxjs/toolkit';
 import { Article } from '../../types/models';
+import publicApi from '../../api/public';
 
-export const storeAll = createAction<Article[]>('article/storeAll');
+export const fetchAll = createAsyncThunk('articlkle/fetchAll', async () => {
+  try {
+    const res = await publicApi.article.getAll();
+    return res.data
+  } catch (err) {
+    // TODO: handle error in redux (like Max maybe? in 3 states ON_SUCCESS , ON_FAIL and ON_LOAD)
+    console.error(err);
+  }
+})
 export const store = createAction<Article>('article/store');
 export const isEditing = createAction<boolean>('article/isEditing');
